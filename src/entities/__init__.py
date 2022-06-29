c2adf05bb1fc038829aae49560fb7a6e86f097e8
inphengon <108392680+inphengon@users.noreply.github.com>
2022-06-29 16:30:30 +0800

    Delete src directory

--- src/entities/__init__.py
+++
@@ -1,6 +0,0 @@
-from entities.base_entity import BaseEntity
-from entities.game_item import GameItem
-from entities.player import Player
-from entities.robot import Robot
-
-__all__ = ["BaseEntity", "GameItem", "Player", "Robot"]
